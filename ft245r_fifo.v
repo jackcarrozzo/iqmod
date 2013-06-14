2e3b16b50784d589d41d361fc564d1149f56edf1
Jack Carrozzo <jack@crepinc.com>
2013-06-14 17:56:34 -0400

    working 1khz AM on 9.375mhz carrier

--- ft245r_fifo.v
+++ ft245r_fifo.v
@@ -1,18 +1,16 @@
-`define CLKSPERSYM 1000 // set i+q after this many clock cycles
+`define CLKSPERSYM 50000000 // set i+q after this many clock cycles
 
 module ft245r_fifo (
-	i,      // out 
-	q,      // out
-	rd_,	// out: read strobe (
-	wr,		// out: write strobe
-	usbdata,// in: the data bus from the ft245r
-	txe_,   // in: tx enable (when high, dont write)
-	rxf_,	// in: read ready (when high, dont read)
-	clk     // in
+	lastbyte,	// out
+	rd_,			// out: read strobe (latches the value on the ft)
+	wr,				// out: write strobe
+	usbdata,	// in: the data bus from the ft245r
+	txe_,   	// in: tx enable (when high, dont write)
+	rxf_,			// in: read ready (when high, dont read)
+	clk     	// in
 );
 
-output signed [7:0]  i;
-output signed [7:0]  q;
+output [7:0] lastbyte;
 output rd_;
 output wr;
 
@@ -21,49 +19,43 @@ input txe_;
 input rxf_;
 input clk;
 
-reg [15:0] counter=0;
+reg [31:0] counter=0;
 reg [2:0] state=0;
+reg [7:0] lastbyte=0;
 
-reg	readval=0; // 0 i, 1 q
+reg rd_=1;
+reg wr =0;
 
-wire[7:0] i;
-wire[7:0] q;
-
-initial begin
-	// generate a carrier at least
-	i=127;
-	q=0;
-	readval=0;
-end
-
-always @(negedge clk) begin
-	counter=counter+1;
-
-	if counter==`CLKSPERSYM begin
-		counter=0;
+always @(negedge clk) 
+	begin
+		counter=counter+1;
 
 		// http://www.mouser.com/ds/2/163/DS_FT245R-19492.pdf
 		// pg 13			
 		case (state)
-			0: begin // start. if !rxf, clear rd_
+			0: begin // start. if !rxf (theres data to read), clear rd_
 				if (0==rxf_) begin
 					rd_=0;
 					state=1;
 				end
 			end
-			1: begin // wait for clk
+			1: begin // wait for clk (TODO: is this still needed?)
 				state=2;
 			end
 			2: begin // fetch value
-				if (readval) q=usbdata;
-				else 		 i=usbdata;
-
-				readval=~readval;
+				lastbyte=usbdata;
 				state=3;
 			end
 			3: begin // set rd_
 				rd_=1;
-				state=0;
+				state=4;
+			end
+			4: begin // wait for next counter event
+				if (counter>=`CLKSPERSYM)
+					begin
+						counter=0; // reset counter
+						state=0;   // start read sequence on next clock
+					end
 			end
 		endcase
 	end
--- modulator.v
+++ modulator.v
@@ -2,33 +2,74 @@ module modulator (
 	dacval, // out: the value written to the dac
 	loclk,  // out: local osc for mixer
 	synthclk, // out: clk to the dac
-	locked, //out
-	clk    // in
+	pll_locked, //out
+	usb_rd_, // out: ftdi 
+	usb_wr,  // out: ftdi
+	status,  //out: status liights
+	clk,    // in
+	usb_bus, // in from ftdi: databus
+	usb_txe_, // ftdi ready to transmid
+	usb_rxf_ // ftdi ready for read 
 );
 
-output [9:0] dacval;
-output synthclk;
-output loclk;
-output locked;
+output [9:0] 	dacval;
+output 				synthclk;
+output 				loclk;
+output 				pll_locked;
+output 				usb_rd_;
+output 				usb_wr;
+output [7:0]  status;
 
-input		 clk;
+input		 		clk;
+input [7:0] usb_bus;
+input 			usb_txe_;
+input				usb_rxf_;
 
 reg [3:0]   i;
 reg [3:0]   q;
+wire [7:0]		usbval;
+reg [7:0] 	status;
 
 reg					pllena;
 reg      		areset;
 
+wire [7:0] 	usb_bus;
+wire 				usb_rd_;
+wire 				usb_wr;
+wire 				usb_txe_;
+wire 				usb_rxf_;
+
 iqmod iq0(dacval,i,q,synthclk);
-thepll pll0(areset,clk,pllena,synthclk,loclk,locked);
+thepll pll0(areset,clk,pllena,synthclk,loclk,pll_locked);
+ft245r_fifo ftdi0(usbval,usb_rd_,usb_wr,usb_bus,usb_txe_,usb_rxf_,clk);
 
 initial begin
 	i=15;
-	q=0;
+	q=7;
+
+	status=8'hff;
 
 	pllena=1;
 	areset=0;
 end
+
+reg [31:0] c;
+always @(posedge clk) // send am 1khz tone
+	begin
+		c=c+1;
+		if (c>=50000)
+			begin
+				c=0;
+				status=~status;
+				i=7+status[2:0];
+			end
+	end
+
+//always @(usbval)
+//	begin
+//		i=usbval[7:4];
+//		q=usbval[3:0];
+//	end
 
 endmodule
 
